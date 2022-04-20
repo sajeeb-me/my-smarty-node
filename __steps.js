3b385341843cee3aa0a5aef62d5c4b8a0b9509a8
sdshuvo98 <hellosdshuvo@gamil.com>
2022-04-20 14:57:34 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,25 @@
+/**
+ * 1. create a folder (manually or mkdir)
+ * 2. open command line to that folder
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js
+ * -----------------------
+ * For Express / node server
+ * -----------------------
+ * 1. require express
+ * 2. create app variable
+ * 3. declare port
+ * 4. app.get ('/')
+ * 5. listen to port
+ * 6. node index.js
+ * 7. check the browser for that port
+ * 
+ * -----------------------
+ * get 
+ * -----------------------
+ * 1. cors middleware
+ * -----------------------
+ * post 
+ * -----------------------
+*/
